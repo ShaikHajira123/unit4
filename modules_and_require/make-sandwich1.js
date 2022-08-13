d722f82a9cc650c9168a657491c41369756e6091
ShaikHajira123 <97529565+ShaikHajira123@users.noreply.github.com>
2022-08-13 21:32:59 +0530

    Initial commit

---
+++ modules_and_require/make-sandwich1.js
@@ -0,0 +1,21 @@
+// const makeSandwich2 = require("./make-sandwich2");
+
+const password = { task: "secret" };
+
+function makeSandwich() {
+ // console.log(`not Making ${makeSandwich2()}`);
+  console.log("password", password);
+}
+
+module.exports = { makeSandwich, password };
+
+// function cook1(){
+//   console.log("items1")
+// }
+// function cook(){
+//   console.log("items")
+// }
+// module.exports={cook,cook1}
+
+// const makeSandwich2=require("./make-sandwich2")
+// console.log(`dont want ${makeSandwich2()}`)
---
+++ modules_and_require/make-sandwich2.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+  console.log(" Making Sandwich 2");
+  return "Sandwich 2";
+};
+// module.exports=function(){
+//   console.log("cook items")
+//   return "cook items"
+// }
